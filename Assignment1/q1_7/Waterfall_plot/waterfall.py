84519464572b3c1e4e58ad1a0001cb78b9d75284
Aakash Gupta <154510675+sky-gupta@users.noreply.github.com>
2024-08-25 22:05:17 +0530

    waterfall.py

--- Assignment1/q1_7/Waterfall_plot/waterfall.py
+++ Assignment1/q1_7/Waterfall_plot/waterfall.py
@@ -1,31 +1,3 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# # Parameters for synthetic data
-# n_timepoints = 50  # Number of time points
-# n_frequencies = 100  # Number of frequency points
-# time = np.linspace(0, 1, n_timepoints)  # Simulated time axis (0 to 1 second)
-# frequency = np.linspace(0, 500, n_frequencies)  # Frequency axis (0 to 500 Hz)
-
-# # Generate synthetic spectrogram data (linear magnitude)
-# spectrogram = np.sin(2 * np.pi * frequency[:, np.newaxis] * time) * np.exp(-time * 3)
-
-# # Plotting
-# plt.figure(figsize=(10, 6))
-# offset = 5  # Vertical offset between lines
-
-# for i in range(n_timepoints):
-#     # Each time slice as a vertical stack, offset by its index
-#     plt.plot(frequency, spectrogram[:, i] + i * offset, color='blue')
-#     # Optionally fill under the curve for better visual effect
-#     plt.fill_between(frequency, i * offset, spectrogram[:, i] + i * offset, color='blue', alpha=0.3)
-
-# plt.xlabel('Frequency (Hz)')
-# plt.ylabel('Linear Magnitude (Uncal.) + Offset')
-# plt.title('2D Waterfall Plot of Spectrogram Data')
-# plt.grid(True)
-# plt.show()
-
 import matplotlib.pyplot as plt
 import numpy as np
 
