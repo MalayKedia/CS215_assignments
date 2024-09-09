98d8c62e0288f4823a4cccc9885982a258524d72
sky gupta <aksgupta2131@gmail.com>
2024-09-09 15:46:18 +0530

    Modified image

--- Assignment2/q2/2c.py
+++ Assignment2/q2/2c.py
@@ -1,29 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+import math
 
-def sample(loc, scale):
-    uniform_sample = np.random.uniform(0, 1)
-    gaussian_sample = norm.ppf(uniform_sample, loc=loc, scale=scale)
+def sample(loc, scale, size):
+    uniform_sample = np.random.uniform(0, 1, size)
+    gaussian_sample = norm.ppf(uniform_sample, loc=loc, scale=math.sqrt(scale))
     return gaussian_sample
 
 N = 10**5
 params = [(0, 0.2), (0, 1.0), (0, 5.0), (-2, 0.5)]
 
 gaussian_samples=[]
-for i in range(N):
-    entry=[]
-    for param in params:
-        entry.append(sample(param[0], param[1]))
-    gaussian_samples.append(entry)
+for param in params:
+    gaussian_samples.append(sample(param[0], param[1], N))
     
-gaussian_samples=np.array(gaussian_samples).T
+gaussian_samples=np.array(gaussian_samples)
 
 plt.figure(figsize=(10, 8))
-plt.hist(gaussian_samples[0], bins=500, density=True, alpha=0.5, label="N(0, 0.2)", color='blue')
-plt.hist(gaussian_samples[1], bins=500, density=True, alpha=0.5, label="N(0, 1.0)", color='red')
-plt.hist(gaussian_samples[2], bins=500, density=True, alpha=0.5, label="N(0, 5.0)", color='yellow')
-plt.hist(gaussian_samples[3], bins=500, density=True, alpha=0.5, label="N(-2, 0.5)", color='green')
+plt.xlim(-6,6)
+plt.hist(gaussian_samples[0], bins='auto', density=True, alpha=0.5, label=r'$N(\mu=0, \sigma^2=0.2)$', color='blue')
+plt.hist(gaussian_samples[1], bins='auto', density=True, alpha=0.5, label=r'$N(\mu=0, \sigma^2=1.0)$', color='red')
+plt.hist(gaussian_samples[2], bins='auto', density=True, alpha=0.5, label=r'$N(\mu=0, \sigma^2=5.0)$', color='yellow')
+plt.hist(gaussian_samples[3], bins='auto', density=True, alpha=0.5, label=r'$N(\mu=-2, \sigma^2=0.5)$', color='green')
 plt.legend()
 plt.title("Histogram of samples from different Gaussian distributions")
 plt.xlabel("Value")
